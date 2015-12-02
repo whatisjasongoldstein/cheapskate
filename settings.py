1cf7c06f7695d3a50b2586c0f0e1fc8564e25159
Jason Goldstein <jason@betheshoe.com>
2015-12-01 22:20:56 -0500

    Probably don't need this

--- settings.py
+++ settings.py
@@ -73,13 +73,6 @@ STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = os.environ["CHEAPSKATE_SECRET_KEY"]
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-    'django.template.loaders.eggs.Loader',
-)
-
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
