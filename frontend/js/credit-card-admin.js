2eab362adf6c8223b0061e3a5fda09c00b95c37a
Jason Goldstein <jason@betheshoe.com>
2017-05-30 15:36:08 -0400

    Remove FontAwesome

--- frontend/js/credit-card-admin.js
+++ frontend/js/credit-card-admin.js
@@ -76,7 +76,7 @@ CreditCardChargeFilter.prototype.setupEvents = function() {
 CreditCardChargeFilter.prototype.syncAmount = function() {
   let sum = this.data.filter((d) => d.selected === true)
     .map((d) => d.amount)
-    .reduce((total, current) => total + current);
+    .reduce((total, current) => total + current, 0);
 
   let diff = sum - this.amountEl.value;
   this.component.querySelector(".status b").textContent = diff.toFixed(2);
@@ -96,7 +96,16 @@ CreditCardChargeFilter.prototype.render = function(context) {
              <td>${ obj.date }</td>
              <td>
                <a href="${ obj.url }" target="_blank">
-                 <i class="fa fa-chevron-circle-right"></i>
+                  <svg width="1792" height="1792" viewBox="0 0 1792 1792" 
+                    xmlns="http://www.w3.org/2000/svg"><path d="M1280 896q0 14-9 23l-320 
+                    320q-9 9-23 9-13 0-22.5-9.5t-9.5-22.5v-192h-352q-13 
+                    0-22.5-9.5t-9.5-22.5v-192q0-13 9.5-22.5t22.5-9.5h352v-192q0-14 
+                    9-23t23-9q12 0 24 10l319 319q9 9 9 23zm160 
+                    0q0-148-73-273t-198-198-273-73-273 73-198 198-73 273 73 
+                    273 198 198 273 73 273-73 198-198 73-273zm224 0q0 209-103 
+                    385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 
+                    103-385.5 279.5-279.5 385.5-103 385.5 103 
+                    279.5 279.5 103 385.5z"/></svg>
                </a>
              </td>
            </tr>
--- settings.py
+++ settings.py
@@ -29,6 +29,10 @@ ALLOWED_HOSTS = [
     "cheapskate.scruffylogic.com",
 ]
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
