89713ea14545d8c4ed6b5fa420e4e9c359d10f4e
Jason Goldstein <jason@betheshoe.com>
2017-07-15 22:45:41 -0400

    Month tests

--- cheapskate/tests/test_dashboard.py
+++ cheapskate/tests/test_dashboard.py
@@ -1,3 +1,4 @@
+import datetime
 from django.test import TestCase
 
 from ..dashboard import (Month, Dashboard,
@@ -114,21 +115,111 @@ class DashboardHelpersTests(TestCase):
 
 
 class MonthTests(TestCase):
-    
-    def test_basic_month(self):
+
+    def setUp(self):
+        self.exp_category, _ = ExpenseCategory.objects.get_or_create(title="Foo")
+        self.inc_category, _ = IncomeCategory.objects.get_or_create(title="Bar")
+
+        self.cc, _ = Account.objects.get_or_create(title="CC", kind="cc")
+        self.bank, _ = Account.objects.get_or_create(title="CC", kind="checking")
+
+        # Test charges for months will be in Feb. 2017
+        Charge.objects.create(
+            title="Charge1",
+            amount=100,
+            date=datetime.date(2017, 2, 2),
+            category=self.exp_category,
+            account=self.cc,
+        )
+
+        Charge.objects.create(
+            title="Charge2",
+            amount=25,
+            date=datetime.date(2017, 2, 25),
+            category=self.exp_category,
+            account=self.cc,
+        )
+
+        Charge.objects.create(
+            title="Some other time",
+            amount=1000,
+            date=datetime.date(2017, 1, 1),
+            category=self.exp_category,
+            account=self.cc,
+        )
+
+        Deposit.objects.create(
+            title="Deposit 1",
+            amount=500,
+            date=datetime.date(2017, 2, 19),
+            account=self.bank,
+            category=self.inc_category,
+        )
+
+        # Create two months
+        self.empty_month = Month(1, 2015,
+            income_categories=[self.inc_category],
+            expense_categories=[self.exp_category])
+        self.full_month = Month(2, 2017,
+            income_categories=[self.inc_category],
+            expense_categories=[self.exp_category])
+
+    def test_empty_month_totals(self):
         """
-        Default properties, no data.
+        Should all be zero
         """
-        month = Month(1, 2017)
-        self.assertEqual(month.expense_categories, [])
-        self.assertEqual(month.income_categories, [])
-        self.assertEqual(month.index, 1)
-        self.assertEqual(month.name, "January")
-        self.assertEqual(month.totals, {
+        self.assertEqual(self.empty_month.totals, {
             "expenses": 0,
             "income": 0,
             "net": 0,
         })
 
+    def test_empty_month_categories_totals(self):
+        """
+        Should be all zero.
+        """
+        self.assertEqual(self.empty_month.expense_categories, [
+            {"title": self.exp_category.title, "total": 0},
+        ])
+
+        self.assertEqual(self.empty_month.income_categories, [
+            {"title": self.inc_category.title, "total": 0},
+        ])
+
+    def test_month_names(self):
+        """
+        Pulled from calendar by their index.
+        """
+        self.assertEqual(self.empty_month.name, "January")
+        self.assertEqual(self.full_month.name, "February")
+
+    def test_full_month_totals(self):
+        """
+        Should add up records from the 
+        given month.
+        """
+        self.assertEqual(self.full_month.totals, {
+            'expenses': 125.0,
+            'income': 500.0,
+            'net': 375.0,
+            'percent': 75,
+        })
+
+    def test_month_category_totals(self):
+        """
+        Records should be totaled and avaialble
+        as a list of categories.
+        """
+        self.assertEqual(self.full_month.expense_categories, [
+            {"title": self.exp_category.title, "total": 125.0},
+        ])
+
+        self.assertEqual(self.full_month.income_categories, [
+            {"title": self.inc_category.title, "total": 500.0},
+        ])
+
+
+
 class DashboardTests(TestCase):
     pass
+
